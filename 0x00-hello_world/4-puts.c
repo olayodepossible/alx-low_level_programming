650e1b462f5b1aa7ed7e015eb36c7a4ba8f312bc
Akinfemiwa <possibleolayode5344@gmail.com>
2022-09-08 16:55:06 +0000

    create a script that prints a phrase exactly the way it was written

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - prints the phrase "Programing is like building a multilingual puzzle"
+ * Return: 0 if it run with no error, else non-zero digit
+*/
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle" );
+	return (0);
+}
