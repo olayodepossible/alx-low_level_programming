b992484643c60d565f7349121cf009c34a34329a
Akinfemiwa <possibleolayode5344@gmail.com>
2022-09-09 19:00:05 +0000

    fix wrong header include

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 
 /*
 * main -  prints all the numbers of base 16 in lowercase
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 
 /*
 * main -  prints all single digit numbers of base 10 starting from 0
