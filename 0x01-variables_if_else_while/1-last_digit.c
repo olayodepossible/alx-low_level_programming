278aa0c6e47d14369b966d31ff2678dc2a8f4ef9
Akinfemiwa <possibleolayode5344@gmail.com>
2022-09-09 10:50:48 +0000

    create a file that print the last digit of the number stored in the variable n

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/*
+* main - print the last digit of the number stored in the variable n
+* Return: 0 provided no error
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int n_modulo = n % 10;
+
+	if (n_modulo == 0)
+		printf("Last digit of %d is %d and is 0\n", n, n_modulo);
+	else
+		if (n_modulo > 5)
+			printf("Last digit of %d is %d and is greater than 5\n", n, n_modulo);
+		else
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n_modulo);
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,4 +1,5 @@
 
 | Script Files | Description |
 | ----------- | ----------- |
-| 0-positive_or_negative.c |  A script that print whether the number stored in the variable n is positive or negative |
+| 0-positive_or_negative.c | Prints whether the number stored in the variable n is positive or negative |
+|1-last_digit.c | Prints the last digit of the number stored in the variable n |
