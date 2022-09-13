0cae9d273d9268a221ab1ec89ed2ba558c004846
Akinfemiwa <possibleolayode5344@gmail.com>
2022-09-13 18:28:42 +0000

    fix synthax error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,7 +30,7 @@ void print_times_table(int n)
 				}
 				else if (k >= 10 && k < 100)
 				{
-					_putchar(',')
+					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
