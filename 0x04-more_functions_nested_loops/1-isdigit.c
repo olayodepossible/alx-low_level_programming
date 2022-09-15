6e720d9c00b0cde34fe29c400a559f5b3bb9cc31
Akinfemiwa <possibleolayode5344@gmail.com>
2022-09-15 15:10:52 +0000

    create a file that checks for a digit from 0 to 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _isdigit - checks for a digit (0 through 9)
+* @c: parameter to check
+* Return: 1 if success, 0 otherwise
+*/
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 | Files | Description |
 | ----------- | ----------- |
 | 0-isupper.c | Checks for uppercase character |
+| 1-isdigit.c | Checks for a digit(0 through 9)|
