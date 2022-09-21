09b90c10cf094f2f01a6b4a9c0f1c8d0222c152d
Akinfemiwa <possibleolayode5344@gmail.com>
2022-09-21 19:59:21 +0000

    fix compile error

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,12 @@ char *string_toupper(char *c)
 
 	while (*(c + i) != '\0')
 	{
-		if ((*(c + i) >= 97 && (*(c + i) < 123))
+		if ((*(c + i) >= 97 && (*(c + i) <= 122))
 		{
 			*(c + i) =  *(c + i) - 32;
 		}
 		i++;
 	}
+
+	return (c)
 }
