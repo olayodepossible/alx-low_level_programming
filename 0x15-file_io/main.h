697ef75bfebd4f7b9e8d04dd533e5b155201de4c
Akinfemiwa <possibleolayode5344@gmail.com>
2022-11-01 02:28:36 +0000

    fix betty checker issue

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,12 @@
 #ifndef _MAIN_H_
-#define _MAIN_H__
+#define _MAIN_H_
 
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
