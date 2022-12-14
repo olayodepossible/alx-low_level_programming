1ba603235b7ae118c2bede09bed120ffbd3b4c3a
Akinfemiwa <possibleolayode5344@gmail.com>
2022-12-14 23:34:59 +0000

    fix build failure

--- 0x18-dynamic_libraries/myprintf.c
+++ 0x18-dynamic_libraries/myprintf.c
@@ -9,8 +9,8 @@
  */
 int printf(const char *format, ...)
 {
-	write(1, "9 8 10 24 75 -9\n", 17);
-	write(1, "Congratulations, you win the Jackpot!\n", 38);
+	write(STDERR_FILENO, "9 8 10 24 75 -9\n", 17);
+	write(STDERR_FILENO, "Congratulations, you win the Jackpot!", 38);
 	exit(EXIT_SUCCESS); /* print once and exit*/
 
 }
