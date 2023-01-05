7bd39cffc4d6fabd9e4267d12e69d629553c44a2
Olayodepossible <possibleolayode5344@gmail.com>
2023-01-05 19:32:36 +0100

    update delete function

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -5,28 +5,21 @@
  * @ht: A pointer to a hash table.
  */
 
-void hash_table_delete(hash_table_t *ht)
-{
-
-	hash_table_t *head = ht;
-	hash_node_t *node, *tmp;
-	unsigned long int i;
-
-	for (i = 0; i < ht->size; i++)
-	{
-		if (ht->array[i] != NULL)
-		{
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				tmp = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = tmp;
-			}
+void delete_hash_table(hash_table *ht) {
+    unsigned long int i;
+    // Free each bucket
+    for (i = 0; i < ht->size; i++) {
+	if(ht->buckets[i] != NULL){    
+		bucket *b = ht->buckets[i];
+		while (b) {
+		    bucket *next = b->next;
+		    free(b);
+		    b = next;
 		}
 	}
-	free(head->array);
-	free(head);
+    }
+    // Free the array of buckets
+    free(ht->buckets);
+    // Free the hash table structure itself
+    free(ht);
 }
