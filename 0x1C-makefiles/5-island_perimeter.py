804d07f3eb2de72b3b2917f5d9c7c1732205ef56
Akinfemiwa <possibleolayode5344@gmail.com>
2023-02-23 12:11:32 +0000

    update some task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,24 +15,17 @@ def island_perimeter(grid):
     """
     p = 0
     height = len(grid)
-    width = len(grid[0])
-    for lth in range(height):
-        for bth in range(width):
-            if (grid[lth][bth] == 1):
-                if (lth == 0):
+		if grid != []:
+    		width = len(grid[0])
+    for a in range(height):
+        for b in range(width):
+            if (grid[a][b] == 1):
+                if (a - 1) == -1 or grid[a - 1][b] == 0:
                     p += 1
-                if (grid[lth - 1][bth]) == 0:
-                    p += 1  # top surrounded by water
-                if (lth == (height - 1)):
+                if (a + 1) == nrows or grid[a + 1][b] == 0:
                     p += 1
-                if (grid[lth + 1][bth]) == 0:
-                    p += 1  # bottom surrounded by water
-                if (bth == 0):
+                if (b - 1) == -1 or grid[a][b - 1] == 0:
                     p += 1
-                if (grid[lth][bth - 1] == 0):
-                    p += 1  # left surrounded by water
-                if (bth == (width - 1)):
+                if (b + 1) == ncolumns or grid[a][b + 1] == 0:
                     p += 1
-                if (grid[lth][bth + 1]) == 0:
-                    p += 1  # right surrounded by water
     return p
